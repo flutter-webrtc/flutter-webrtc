f360c96c7c517f85d42e0736215470d7e16012f5
段维伟 <duanweiwei1982@gmail.com>
2018-05-12 00:49:41 +0800

    Add video rotation and video size change event callbacks.

--- ios/Classes/FlutterRTCVideoViewManager.m
+++ ios/Classes/FlutterRTCVideoViewManager.m
@@ -9,15 +9,30 @@
     CGSize _renderSize;
     CGSize _frameSize;
     CVPixelBufferRef _pixelBufferRef;
+    RTCVideoRotation _rotation;
+    FlutterEventChannel* _eventChannel;
 }
 
+@synthesize textureId  = _textureId;
+@synthesize registry = _registry;
+@synthesize eventSink = _eventSink;
+
 - (instancetype)initWithSize:(CGSize)renderSize
-                  onNewFrame:(void(^)(void))onNewFrame {
+         withTextureRegistry:(id<FlutterTextureRegistry>)registry
+                   messenger:(NSObject<FlutterBinaryMessenger>*)messenger{
     self = [super init];
     if (self){
         _renderSize = renderSize;
-        _onNewFrame = onNewFrame;
+        _registry = registry;
         _pixelBufferRef = nil;
+        _eventSink = nil;
+        _rotation  = -1;
+        _textureId  = [registry registerTexture:self];
+        /*Create Event Channel.*/
+        _eventChannel = [FlutterEventChannel
+                                       eventChannelWithName:[NSString stringWithFormat:@"cloudwebrtc.com/WebRTC/Texture%lld", _textureId]
+                                       binaryMessenger:messenger];
+        [_eventChannel setStreamHandler:self];
     }
     return self;
 }
@@ -29,34 +44,15 @@
 }
 
 - (CVPixelBufferRef)copyPixelBuffer {
-    CVBufferRetain(_pixelBufferRef);
-    return _pixelBufferRef;
+    if(_pixelBufferRef != nil){
+        CVBufferRetain(_pixelBufferRef);
+        return _pixelBufferRef;
+    }
+    return nil;
 }
 
-/**
- * Implements the setter of the {@link #mirror} property of this
- * {@code RTCVideoView}.
- *
- * @param mirror The value to set on the {@code mirror} property of this
- * {@code RTCVideoView}.
- */
-- (void)setMirror:(BOOL)mirror {
-    if (_mirror != mirror) {
-        _mirror = mirror;
-    }
-}
-
-/**
- * Implements the setter of the {@link #objectFit} property of this
- * {@code RTCVideoView}.
- *
- * @param objectFit The value to set on the {@code objectFit} property of this
- * {@code RTCVideoView}.
- */
-- (void)setObjectFit:(RTCVideoViewObjectFit)objectFit {
-    if (_objectFit != objectFit) {
-        _objectFit = objectFit;
-    }
+-(void)dispose{
+    [_registry unregisterTexture:_textureId];
 }
 
 /**
@@ -83,12 +79,32 @@
 #pragma mark - RTCVideoRenderer methods
 - (void)renderFrame:(RTCVideoFrame *)frame {
     
-    //TODO: got a frame => scale to _renderSize => correct rotation => convert to BGRA32 pixelBufferRef
+    //TODO: got a frame => scale to _renderSize => convert to BGRA32 pixelBufferRef
     
     [frame CopyI420BufferToCVPixelBuffer:_pixelBufferRef];
     
+    __weak RTCVideoView *weakSelf = self;
+    
+    if(frame.rotation != _rotation){
+        dispatch_async(dispatch_get_main_queue(), ^{
+            RTCVideoView *strongSelf = weakSelf;
+            if(strongSelf.eventSink){
+                strongSelf.eventSink(@{
+                                   @"event" : @"didTextureChangeRotation",
+                                   @"id": @(strongSelf.textureId),
+                                   @"rotation": @(frame.rotation),
+                                   });
+            }
+        });
+
+        _rotation = frame.rotation;
+    }
+    
     //Notify the Flutter new pixelBufferRef to be ready.
-    dispatch_async(dispatch_get_main_queue(), self.onNewFrame);
+    dispatch_async(dispatch_get_main_queue(), ^{
+        RTCVideoView *strongSelf = weakSelf;
+        [strongSelf.registry textureFrameAvailable:strongSelf.textureId];
+    });
 }
 
 /**
@@ -99,21 +115,50 @@
 - (void)setSize:(CGSize)size {
     if(_pixelBufferRef == nil || (size.width != _frameSize.width || size.height != _frameSize.height))
     {
+        if(_pixelBufferRef){
+            CVBufferRelease(_pixelBufferRef);
+        }
         CVPixelBufferCreate(kCFAllocatorDefault,
                             size.width, size.height,
                             kCVPixelFormatType_32BGRA,
                             NULL, &_pixelBufferRef);
     }
+    
+    __weak RTCVideoView *weakSelf = self;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        RTCVideoView *strongSelf = weakSelf;
+        if(strongSelf.eventSink){
+            strongSelf.eventSink(@{
+                    @"event" : @"didTextureChangeVideoSize",
+                    @"id": @(strongSelf.textureId),
+                    @"width": @(size.width),
+                    @"height": @(size.height),
+                    });
+        }
+    });
     _frameSize = size;
 }
 
+#pragma mark - FlutterStreamHandler methods
+
+- (FlutterError* _Nullable)onCancelWithArguments:(id _Nullable)arguments {
+    _eventSink = nil;
+    return nil;
+}
+
+- (FlutterError* _Nullable)onListenWithArguments:(id _Nullable)arguments
+                                       eventSink:(nonnull FlutterEventSink)sink {
+    _eventSink = sink;
+    return nil;
+}
 @end
 
 @implementation FlutterWebRTCPlugin (RTCVideoViewManager)
 
-- (RTCVideoView *)createWithSize:(CGSize)size onNewFrame:(void(^)(void))onNewFrame{
-    RTCVideoView *v = [[RTCVideoView alloc] initWithSize:size onNewFrame:onNewFrame];
-    return v;
+- (RTCVideoView *)createWithSize:(CGSize)size
+                    withTextureRegistry:(id<FlutterTextureRegistry>)registry
+                       messenger:(NSObject<FlutterBinaryMessenger>*)messenger{
+    return [[RTCVideoView alloc] initWithSize:size withTextureRegistry:registry messenger:messenger];
 }
 
 -(void)setStreamId:(NSString*)streamId view:(RTCVideoView*)view {
