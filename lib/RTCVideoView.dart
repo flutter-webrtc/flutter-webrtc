721fedc03b5bf8c94b73c14775ef3b9d7157c630
段维伟 <duanweiwei1982@gmail.com>
2018-05-13 01:52:32 +0800

    Update.

--- lib/RTCVideoView.dart
+++ lib/RTCVideoView.dart
@@ -1,7 +1,9 @@
 import 'package:webrtc/MediaStream.dart';
 import 'package:webrtc/WebRTC.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
 import 'dart:async';
+import 'dart:math';
 
 enum RTCVideoViewObjectFit {
   RTCVideoViewObjectFitContain,
@@ -16,9 +18,10 @@ class RTCVideoRenderer {
   MethodChannel _channel = WebRTC.methodChannel();
   int _textureId;
   int _rotation = 0;
-  double _width, _height;
+  double _width = 0.0, _height = 0.0;
   bool _mirror = false;
   bool _muted = false;
+  MediaStream _srcObject;
   RTCVideoViewObjectFit _objectFit =
       RTCVideoViewObjectFit.RTCVideoViewObjectFitContain;
   StreamSubscription<dynamic> _eventSubscription;
@@ -34,15 +37,11 @@ class RTCVideoRenderer {
         .listen(eventListener, onError: errorListener);
   }
 
-  int get renderId => _textureId;
-
   int get rotation => _rotation;
 
   double get width => _width;
 
   double get height => _height;
-
-  bool get isInitialized => _textureId != null;
 
   set muted(bool muted) {
     _muted = muted;
@@ -59,6 +58,7 @@ class RTCVideoRenderer {
   set srcObject(MediaStream stream) {
     _channel.invokeMethod('videoRendererSetSrcObject',
         <String, dynamic>{'textureId': _textureId, 'streamId': stream.id});
+    _srcObject = stream;
   }
 
   Future<Null> dispose() async {
@@ -91,5 +91,65 @@ class RTCVideoRenderer {
 
   void errorListener(Object obj) {
     final PlatformException e = obj;
+  }
+}
+
+class RTCVideoView extends StatefulWidget {
+  final RTCVideoRenderer renderer;
+  RTCVideoView(this.renderer) {}
+  @override
+  _RTCVideoViewState createState() => new _RTCVideoViewState(renderer);
+}
+
+class _RTCVideoViewState extends State<RTCVideoView> {
+  final RTCVideoRenderer renderer;
+  _RTCVideoViewState(this.renderer);
+  @override
+  void initState() {
+    super.initState();
+    renderer.onVideoRotationChange = (int textureId, int rotation) {
+      setState(() {});
+    };
+    renderer.onVideoSizeChange = (int textureId, double width, double height) {
+      setState(() {});
+    };
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var width = 0.0;
+    var height = 0.0;
+    if(context.findRenderObject() != null){
+      final BoxConstraints constraints = context.findRenderObject().constraints;
+      double scale = 1.0;
+      if(renderer.rotation == 90 || renderer.rotation == 270){
+        width = min(renderer.width, renderer.height);
+        height = max(renderer.width, renderer.height);
+        scale = min(constraints.minWidth / width, constraints.minHeight / height);
+      }else {
+        width = max(renderer.width, renderer.height);
+        height = min(renderer.width, renderer.height);
+        scale = max(constraints.minWidth / width, constraints.minHeight / height);
+      }
+      width *= scale;
+      height *= scale;
+    }
+
+    return new Center(
+        child: new Container(
+            child: new Center(
+              child: (this.renderer._textureId == null || this.renderer._srcObject == null)
+                  ? new Container()
+                  : new Container(
+                          width: width,
+                          height: height,
+                          child: new Texture(textureId: this.renderer._textureId),
+                        )
+            )));
   }
 }
