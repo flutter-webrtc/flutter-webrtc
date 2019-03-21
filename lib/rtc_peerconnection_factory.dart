e2f23d84dd700daae534a432cd813e278c6c61a8
Linus Unnebäck <linus@folkdatorn.se>
2019-03-21 11:18:49 +0100

    Fix indentation in peer connection factory

--- lib/rtc_peerconnection_factory.dart
+++ lib/rtc_peerconnection_factory.dart
@@ -3,24 +3,24 @@ import 'package:flutter/services.dart';
 import 'rtc_peerconnection.dart';
 import 'utils.dart';
 
-Future<RTCPeerConnection> createPeerConnection(Map<String,dynamic> configuration, Map<String,dynamic> constraints) async {
+Future<RTCPeerConnection> createPeerConnection(Map<String, dynamic> configuration, Map<String, dynamic> constraints) async {
   MethodChannel channel = WebRTC.methodChannel();
 
   Map<String, dynamic> defaultConstraints = {
-      "mandatory": {},
-      "optional": [
-        {"DtlsSrtpKeyAgreement": true },
-      ],
-    };
+    "mandatory": {},
+    "optional": [
+      {"DtlsSrtpKeyAgreement": true},
+    ],
+  };
 
-    final Map<dynamic, dynamic> response = await channel.invokeMethod(
-      'createPeerConnection',
-      <String, dynamic>{
-        'configuration': configuration,
-        'constraints': constraints.length == 0? defaultConstraints : constraints
-      },
-    );
+  final Map<dynamic, dynamic> response = await channel.invokeMethod(
+    'createPeerConnection',
+    <String, dynamic>{
+      'configuration': configuration,
+      'constraints': constraints.length == 0 ? defaultConstraints : constraints
+    },
+  );
 
-    String peerConnectionId = response['peerConnectionId'];
-    return new RTCPeerConnection(peerConnectionId);
+  String peerConnectionId = response['peerConnectionId'];
+  return new RTCPeerConnection(peerConnectionId);
 }
