e630506b4714c6822042047c715def13f4e68703
Linus Unnebäck <linus@folkdatorn.se>
2019-04-17 11:32:00 +0200

    Fix self property access in video renderer
    Fixes the following error:
    
    ```
    error: property '_isFirstFrameRendered' not found on object of type 'FlutterRTCVideoRenderer *'; did you mean to access instance variable '_isFirstFrameRendered'?
                self._isFirstFrameRendered = false;
                    ~^
    ```

--- ios/Classes/FlutterRTCVideoRenderer.m
+++ ios/Classes/FlutterRTCVideoRenderer.m
@@ -67,7 +67,7 @@
     RTCVideoTrack *oldValue = self.videoTrack;
     
     if (oldValue != videoTrack) {
-        self._isFirstFrameRendered = false;
+        _isFirstFrameRendered = false;
         if (oldValue) {
             [oldValue removeRenderer:self];
         }
