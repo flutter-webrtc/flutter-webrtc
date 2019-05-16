e59171c0bb59bb373ab435c5f6665c0f4588789b
段维伟 <duanweiwei1982@gmail.com>
2019-05-16 12:16:41 +0800

    Fixed compiler error on ios.

--- ios/Classes/FlutterRTCDataChannel.m
+++ ios/Classes/FlutterRTCDataChannel.m
@@ -134,7 +134,7 @@
     FlutterEventSink eventSink = channel.eventSink;
     if(eventSink) {
         eventSink(@{ @"event" : @"dataChannelStateChanged",
-                     @"id": @(channel.channelId),
+                     @"id": channel.flutterChannelId,
                      @"state": [self stringForDataChannelState:channel.readyState]});
     }
 }
@@ -156,7 +156,7 @@
     FlutterEventSink eventSink = channel.eventSink;
     if(eventSink) {
         eventSink(@{ @"event" : @"dataChannelReceiveMessage",
-                     @"id": @(channel.flutterChannelId),
+                     @"id": channel.flutterChannelId,
                      @"type": type,
                      @"data": (data ? data : [NSNull null])});
     }
