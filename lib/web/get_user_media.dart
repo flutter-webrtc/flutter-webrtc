82e07d340787cb18852508ae19d1a226e28f6de0
Dima Rostopira <rostopiradv@gmail.com>
2019-08-22 10:37:37 +0300

    Create get_user_media.dart

---
+++ lib/web/get_user_media.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+// ignore: uri_does_not_exist
+import 'dart:js' as JS;
+// ignore: uri_does_not_exist
+import 'dart:html' as HTML;
+import 'media_stream.dart';
+import 'utils.dart';
+
+class navigator {
+
+  static Future<MediaStream> getUserMedia(Map<String, dynamic> mediaConstraints) async {
+    final nav = HTML.window.navigator;
+    final jsStream = await nav.getUserMedia(
+        audio: mediaConstraints['audio'] ?? false,
+        video: mediaConstraints['video'] ?? false
+    );
+    print("Got jsStream ${jsStream}");
+    return MediaStream(jsStream);
+  }
+
+  //TODO: test this
+  static Future<MediaStream> getDisplayMedia(Map<String, dynamic> mediaConstraints) async {
+    final nav = HTML.window.navigator;
+    final mediaDevices = nav.mediaDevices;
+    final jsMediaDevices = JS.JsObject.fromBrowserObject(mediaDevices);
+    if (jsMediaDevices.hasProperty(getDisplayMedia)) {
+      final JS.JsObject arg = JS.JsObject.jsify({"video":true});
+      JS.JsObject getDisplayMediaPromise = jsMediaDevices.callMethod('getDisplayMedia',[arg]);
+      final HTML.MediaStream jsStream = await promiseToFuture(getDisplayMediaPromise);
+      return MediaStream(jsStream);
+    } else {
+      return await nav.getUserMedia(
+        video: {"mediaSource":'screen'},
+        audio: mediaConstraints['audio'] ?? false
+      );
+    }
+  }
+
+  //FIXME(rostopira): throws weird error
+  static Future<List<dynamic>> getSources() async {
+    final List devices = await HTML.window.navigator.mediaDevices.enumerateDevices();
+    return devices.cast<dynamic>();
+  }
+
+}
