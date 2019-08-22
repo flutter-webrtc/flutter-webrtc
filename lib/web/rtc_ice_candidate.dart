8910ac0e9e767fa339e822ac796fe1029065bf2a
Dima Rostopira <rostopiradv@gmail.com>
2019-08-22 11:27:34 +0300

    Create rtc_ice_candidate.dart

---
+++ lib/web/rtc_ice_candidate.dart
@@ -0,0 +1,27 @@
+// ignore: uri_does_not_exist
+import 'dart:html' as HTML;
+
+class RTCIceCandidate {
+  String candidate;
+  String sdpMid;
+  int sdpMlineIndex;
+
+  RTCIceCandidate(this.candidate, this.sdpMid, this.sdpMlineIndex);
+  RTCIceCandidate.fromJs(HTML.RtcIceCandidate jsIceCandidate): this(
+    jsIceCandidate.candidate,
+    jsIceCandidate.sdpMid,
+    jsIceCandidate.sdpMLineIndex
+  );
+
+  dynamic toMap() {
+    return {
+      "candidate": candidate,
+      "sdpMid": sdpMid,
+      "sdpMLineIndex": sdpMlineIndex
+    };
+  }
+
+  HTML.RtcIceCandidate toJs() =>
+    HTML.RtcIceCandidate(toMap());
+
+}
