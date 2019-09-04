7a5df7959f526f4ed53e66585306953010e946c0
Dima Rostopira <rostopiradv@gmail.com>
2019-09-04 12:35:01 +0300

    Create web_plugin.dart

---
+++ lib/web_plugin.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+class FlutterWebRTCPlugin {
+
+  static void registerWith(Registrar registrar) {
+    //TODO: register platform view factory
+  }
+
+}
