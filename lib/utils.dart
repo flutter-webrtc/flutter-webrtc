5e40b78cc81b796ab19c5a6430b1271f7d11a932
CloudWebRTC <duanweiwei1982@gmail.com>
2019-11-20 12:00:50 +0800

    Add platformIs[Desktop|Mobile|Web] methods.

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,74 +1,15 @@
-import 'dart:io' show Platform;
+import 'dart:io';
 import 'package:flutter/services.dart';
-import 'enums.dart';
 
 class WebRTC {
-  static const MethodChannel _channel = const MethodChannel('FlutterWebRTC.Method');
+  static const MethodChannel _channel =
+      const MethodChannel('FlutterWebRTC.Method');
   static MethodChannel methodChannel() => _channel;
-}
 
-RTCIceConnectionState iceConnectionStateForString(String state) {
-  switch (state) {
-    case "new":
-      return RTCIceConnectionState.RTCIceConnectionStateNew;
-    case "checking":
-      return RTCIceConnectionState.RTCIceConnectionStateChecking;
-    case "connected":
-      return RTCIceConnectionState.RTCIceConnectionStateConnected;
-    case "completed":
-      return RTCIceConnectionState.RTCIceConnectionStateCompleted;
-    case "failed":
-      return RTCIceConnectionState.RTCIceConnectionStateFailed;
-    case "disconnected":
-      return RTCIceConnectionState.RTCIceConnectionStateDisconnected;
-    case "closed":
-      return RTCIceConnectionState.RTCIceConnectionStateClosed;
-    case "count":
-      return RTCIceConnectionState.RTCIceConnectionStateCount;
-  }
-  return RTCIceConnectionState.RTCIceConnectionStateClosed;
-}
+  static bool get platformIsDesktop =>
+      Platform.isWindows || Platform.isLinux || Platform.isMacOS;
 
-RTCIceGatheringState iceGatheringStateforString(String state) {
-  switch (state) {
-    case "new":
-      return RTCIceGatheringState.RTCIceGatheringStateNew;
-    case "gathering":
-      return RTCIceGatheringState.RTCIceGatheringStateGathering;
-    case "complete":
-      return RTCIceGatheringState.RTCIceGatheringStateComplete;
-  }
-  return RTCIceGatheringState.RTCIceGatheringStateNew;
-}
+  static bool get platformIsMobile => Platform.isIOS || Platform.isAndroid;
 
-RTCSignalingState signalingStateForString(String state) {
-  switch (state) {
-    case "stable":
-      return RTCSignalingState.RTCSignalingStateStable;
-    case "have-local-offer":
-      return RTCSignalingState.RTCSignalingStateHaveLocalOffer;
-    case "have-local-pranswer":
-      return RTCSignalingState.RTCSignalingStateHaveLocalPrAnswer;
-    case "have-remote-offer":
-      return RTCSignalingState.RTCSignalingStateHaveRemoteOffer;
-    case "have-remote-pranswer":
-      return RTCSignalingState.RTCSignalingStateHaveRemotePrAnswer;
-    case "closed":
-      return RTCSignalingState.RTCSignalingStateClosed;
-  }
-  return RTCSignalingState.RTCSignalingStateClosed;
-}
-
-RTCDataChannelState rtcDataChannelStateForString(String state) {
-  switch (state) {
-    case "connecting":
-      return RTCDataChannelState.RTCDataChannelConnecting;
-    case "open":
-      return RTCDataChannelState.RTCDataChannelOpen;
-    case "closing":
-      return RTCDataChannelState.RTCDataChannelClosing;
-    case "closed":
-      return RTCDataChannelState.RTCDataChannelClosed;
-  }
-  return RTCDataChannelState.RTCDataChannelClosed;
+  static bool get platformIsWeb => false;
 }
---
+++ lib/web/utils.dart
@@ -0,0 +1,8 @@
+
+class WebRTC {
+  static bool get platformIsDesktop => false;
+
+  static bool get platformIsMobile => false;
+
+  static bool get platformIsWeb => true;
+}
