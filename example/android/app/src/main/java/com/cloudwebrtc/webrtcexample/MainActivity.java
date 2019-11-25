7ac549cdf459eadee15f7d7c9d6dff426b6811ca
Ivan Gemba <gembaivan@gmail.com>
2019-11-25 13:54:55 +0200

    fix(example for Android);

--- example/android/app/src/main/java/com/cloudwebrtc/webrtcexample/MainActivity.java
+++ example/android/app/src/main/java/com/cloudwebrtc/webrtcexample/MainActivity.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import com.cloudwebrtc.webrtc.FlutterWebRTCPlugin;
 
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+    FlutterWebRTCPlugin.registerWith(this.registrarFor("com.cloudwebrtc.webrtc.FlutterWebRTCPlugin"));
   }
 }
