ff5fcfce785164eb5a0f8b04d1f1d7dd1669be49
CloudWebRTC <duanweiwei1982@gmail.com>
2019-12-24 09:18:09 +0800

    Fixed #158.

--- macos/Classes/FlutterRTCDataChannel.m
+++ macos/Classes/FlutterRTCDataChannel.m
@@ -77,7 +77,7 @@
         dataChannel.delegate = self;
         
         FlutterEventChannel *eventChannel = [FlutterEventChannel
-                                             eventChannelWithName:[NSString stringWithFormat:@"FlutterWebRTC/dataChannelEvent%d", dataChannel.channelId]
+                                             eventChannelWithName:[NSString stringWithFormat:@"FlutterWebRTC/dataChannelEvent%d", [dataChannelId intValue]]
                                              binaryMessenger:messenger];
         
         dataChannel.eventChannel = eventChannel;
