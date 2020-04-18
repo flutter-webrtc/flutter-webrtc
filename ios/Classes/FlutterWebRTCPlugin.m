15d971303f10d73f11706353597d4135afcbee3d
Yurii Cherniavskyi <ych@devlab21.com>
2020-04-18 17:31:24 +0300

    [iOS/macOS] Fix possible method call handling without result callback call
    In this case, the future, which returned for method call, never complete.
    
    The current fix implements the same behavior as in Android implementation.

--- ios/Classes/FlutterWebRTCPlugin.m
+++ ios/Classes/FlutterWebRTCPlugin.m
@@ -377,25 +377,23 @@
         NSString* peerConnectionId = argsMap[@"peerConnectionId"];
         
         RTCPeerConnection *peerConnection = self.peerConnections[peerConnectionId];
-        if (!peerConnection) {
-            return;
+        if (peerConnection) {
+            [peerConnection close];
+            [self.peerConnections removeObjectForKey:peerConnectionId];
+            
+            // Clean up peerConnection's streams and tracks
+            [peerConnection.remoteStreams removeAllObjects];
+            [peerConnection.remoteTracks removeAllObjects];
+            
+            // Clean up peerConnection's dataChannels.
+            NSMutableDictionary<NSNumber *, RTCDataChannel *> *dataChannels = peerConnection.dataChannels;
+            for (NSNumber *dataChannelId in dataChannels) {
+                dataChannels[dataChannelId].delegate = nil;
+                // There is no need to close the RTCDataChannel because it is owned by the
+                // RTCPeerConnection and the latter will close the former.
+            }
+            [dataChannels removeAllObjects];
         }
-        [peerConnection close];
-        [self.peerConnections removeObjectForKey:peerConnectionId];
-        
-        // Clean up peerConnection's streams and tracks
-        [peerConnection.remoteStreams removeAllObjects];
-        [peerConnection.remoteTracks removeAllObjects];
-        
-        // Clean up peerConnection's dataChannels.
-        NSMutableDictionary<NSNumber *, RTCDataChannel *> *dataChannels
-        = peerConnection.dataChannels;
-        for (NSNumber *dataChannelId in dataChannels) {
-            dataChannels[dataChannelId].delegate = nil;
-            // There is no need to close the RTCDataChannel because it is owned by the
-            // RTCPeerConnection and the latter will close the former.
-        }
-        [dataChannels removeAllObjects];
         result(nil);
     } else if ([@"createVideoRenderer" isEqualToString:call.method]){
         NSDictionary* argsMap = call.arguments;
