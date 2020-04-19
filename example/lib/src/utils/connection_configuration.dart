200121fb6a10dc67987c0b13aa479d019d247e83
TruongSinh Tran-Nguyen <i@truongsinh.pro>
2020-04-18 20:20:47 -0500

    known turn's credential

--- example/lib/src/utils/connection_configuration.dart
+++ example/lib/src/utils/connection_configuration.dart
@@ -1,5 +1,16 @@
 Map<String, dynamic> connectionConfiguration = {
   "iceServers": [
     {"url": "stun:stun.l.google.com:19302"},
+    {
+      "urls": [
+        "turn:209.85.235.127:19305?transport=udp",
+        "turn:[2607:f8b0:4003:c19::7f]:19305?transport=udp",
+        "turn:209.85.235.127:19305?transport=tcp",
+        "turn:[2607:f8b0:4003:c19::7f]:19305?transport=tcp"
+      ],
+      "username": "CPKu8vQFEgZTECXgsNYYzc/s6OMTIICjBQ",
+      "credential": "t/+udhmnS2wR3bJutk0A54Ufuxo=",
+      "maxRateKbps": "8000"
+    }
   ]
 };
