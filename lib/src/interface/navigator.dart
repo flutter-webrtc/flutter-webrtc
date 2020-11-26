79e2aa2c2d1a2aad15e69d8b3a2e9955b7c591ba
Louis-Michel Mathurin <mathurin.lm@we-r.app>
2020-11-26 09:53:42 -0500

    Deprecated Navigator methods

--- lib/src/interface/navigator.dart
+++ lib/src/interface/navigator.dart
@@ -2,8 +2,14 @@ import 'media_stream.dart';
 import 'mediadevices.dart';
 
 abstract class Navigator {
+  @Deprecated('use mediadevice.getUserMedia() instead')
   Future<MediaStream> getUserMedia(Map<String, dynamic> mediaConstraints);
+
+  @Deprecated('use mediadevice.getDisplayMedia() instead')
   Future<MediaStream> getDisplayMedia(Map<String, dynamic> mediaConstraints);
+
+  @Deprecated('use mediadevice.enumerateDevices() instead')
   Future<List<dynamic>> getSources();
+
   MediaDevices get mediaDevices;
 }
