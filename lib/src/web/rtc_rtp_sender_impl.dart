5a1faf711bcaf06ce515fe43856f90baaf18a2f7
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2021-09-12 09:06:17 +0900

    Fix: Flutter 2.5.0 `RTCVideoRendererWeb` bug (#681)
    * Use `track` instead of `kind` when both `track` and `kind` are set (#4)
    
    * `getSettings` for Web (#3)
    
    * Fix: init would never be `RTCRtpTransceiverInitWeb`
    
    * Clean up
    
    * Fix: Flutter 2.5.0 `RTCVideoRendererWeb` bug

--- lib/src/web/rtc_rtp_sender_impl.dart
+++ lib/src/web/rtc_rtp_sender_impl.dart
@@ -4,11 +4,11 @@ import 'dart:js_util' as jsutil;
 
 import 'package:flutter/services.dart';
 
-import 'package:flutter_webrtc/flutter_webrtc.dart';
 import '../interface/media_stream_track.dart';
 import '../interface/rtc_dtmf_sender.dart';
 import '../interface/rtc_rtp_parameters.dart';
 import '../interface/rtc_rtp_sender.dart';
+import '../interface/rtc_stats_report.dart';
 import 'media_stream_track_impl.dart';
 import 'rtc_dtmf_sender_impl.dart';
 import 'rtc_rtp_parameters_impl.dart';
