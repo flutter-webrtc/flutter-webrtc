cde014dc325acfc848c103eb2260295179efb97d
cloudwebrtc <duanweiwei1982@gmail.com>
2021-11-05 08:16:44 +0800

    fix: Fix crashes for FlutterRPScreenRecorder stop.

--- common/darwin/Classes/FlutterRPScreenRecorder.h
+++ common/darwin/Classes/FlutterRPScreenRecorder.h
@@ -4,6 +4,9 @@
 
 -(void)startCapture;
 
+// Stops the capture session asynchronously and notifies callback on completion.
+- (void)stopCaptureWithCompletionHandler:(nullable void (^)(void))completionHandler;
+
 -(void)stopCapture;
 
 @end
