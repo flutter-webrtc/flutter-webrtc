0cc6b4bc7feee5fe039ba0c34f787ebfcae3663b
cloudwebrtc <duanweiwei1982@gmail.com>
2022-08-15 13:16:53 +0800

    Tidy up the code.

--- android/src/main/java/com/cloudwebrtc/webrtc/audio/AudioSwitchManager.java
+++ android/src/main/java/com/cloudwebrtc/webrtc/audio/AudioSwitchManager.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.media.AudioManager;
 import android.os.Handler;
 import android.os.Looper;
-import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
