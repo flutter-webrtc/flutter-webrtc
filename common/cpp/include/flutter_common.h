ca14dac823fb6fe661c14274915cd714ff5c2e1b
cloudwebrtc <duanweiwei1982@gmail.com>
2022-08-28 20:54:39 +0800

    update.

--- common/cpp/include/flutter_common.h
+++ common/cpp/include/flutter_common.h
@@ -10,69 +10,145 @@
 #include <flutter/standard_method_codec.h>
 #include <flutter/texture_registrar.h>
 
+typedef flutter::EncodableValue EncodableValue;
+typedef flutter::EncodableMap EncodableMap;
+typedef flutter::EncodableList EncodableList;
+typedef flutter::BinaryMessenger BinaryMessenger;
+typedef flutter::TextureRegistrar TextureRegistrar;
+typedef flutter::PluginRegistrar PluginRegistrar;
+typedef flutter::MethodChannel<EncodableValue> MethodChannel;
+typedef flutter::EventChannel<EncodableValue> EventChannel;
+typedef flutter::EventSink<EncodableValue> EventSink;
+typedef flutter::MethodCall<EncodableValue> MethodCall;
+typedef flutter::MethodResult<EncodableValue> MethodResult;
+
+#if defined(__linux__)
+typedef FlTextureRegistrar TextureRegistrar;
+typedef FlPluginRegistrar PluginRegistrar;
+typedef FlBinaryMessenger BinaryMessenger;
+typedef FlMethodChannel MethodChannel;
+typedef FlEventChannel EventChannel;
+typedef FlMethodCall MethodCall;
+typedef FlValue * EncodableValue;
+typedef FlValue * EncodableMap;
+typedef FlValue * EncodableList;
+typedef FlMethodResult MethodResult;
+
+// nullptr if none
+inline FlValue *findEncodableValue(const FlValue *map, const std::string& key) {
+  FlValue *mapV = const_cast<FlValue *>(map);
+  FlValue *keyV = fl_value_lookup_string(mapV, key.c_str());
+  return keyV;
+}
+
+inline FlValue *findMap(const FlValue *map, const std::string& key) {
+  FlValue *mapV = const_cast<FlValue *>(map);
+  FlValue *keyV = fl_value_lookup_string(mapV, key.c_str());
+  if (fl_value_get_type(keyV) == FL_VALUE_TYPE_MAP) return keyV;
+  return nullptr;
+}
+
+inline FlValue *findList(const FlValue *map, const std::string& key) {
+  FlValue *mapV = const_cast<FlValue *>(map);
+  FlValue *keyV = fl_value_lookup_string(mapV, key.c_str());
+  if (fl_value_get_type(keyV) == FL_VALUE_TYPE_LIST) return keyV;
+  return nullptr;
+}
+
+inline std::string findString(const FlValue *map, const std::string& key) {
+  FlValue *mapV = const_cast<FlValue *>(map);
+  FlValue *keyV = fl_value_lookup_string(mapV, key.c_str());
+  if (fl_value_get_type(keyV) == FL_VALUE_TYPE_STRING)
+    return fl_value_get_string(keyV);
+  return std::string();
+}
+
+// fl_value is int64_t
+inline int findInt(const FlValue *map, const std::string& key) {
+  FlValue *mapV = const_cast<FlValue *>(map);
+  FlValue *keyV = fl_value_lookup_string(mapV, key.c_str());
+  if (fl_value_get_type(keyV) == FL_VALUE_TYPE_INT)
+    return fl_value_get_int(keyV);
+  return -1;
+}
+
+inline int64_t findLongInt(const FlValue *map, const std::string& key) {
+FlValue *mapV = const_cast<FlValue *>(map);
+  FlValue *keyV = fl_value_lookup_string(mapV, key.c_str());
+  if (fl_value_get_type(keyV) == FL_VALUE_TYPE_INT)
+    return fl_value_get_int(keyV);
+  return -1;
+}
+
+inline int toInt(FlValue *inputVal, int defaultVal) {
+  int intValue = defaultVal;
+  FlValueType vtype = fl_value_get_type(inputVal);
+  if (vtype == FL_VALUE_TYPE_INT) {
+    intValue = fl_value_get_int(inputVal);
+  } else if (vtype == FL_VALUE_TYPE_STRING) {
+    intValue = atoi(fl_value_get_string(inputVal));
+  }
+  return intValue;
+}
+#endif
+
 // foo.StringValue() becomes std::get<std::string>(foo)
 // foo.IsString() becomes std::holds_alternative<std::string>(foo)
 
 template <typename T>
-inline bool TypeIs(const flutter::EncodableValue val) {
+inline bool TypeIs(const EncodableValue val) {
   return std::holds_alternative<T>(val);
 }
 
 template <typename T>
-inline const T GetValue(flutter::EncodableValue val) {
+inline const T GetValue(EncodableValue val) {
   return std::get<T>(val);
 }
 
-inline flutter::EncodableValue findEncodableValue(
-    const flutter::EncodableMap& map,
-    const std::string& key) {
-  auto it = map.find(flutter::EncodableValue(key));
+inline EncodableValue findEncodableValue(const EncodableMap& map,
+                                         const std::string& key) {
+  auto it = map.find(EncodableValue(key));
   if (it != map.end())
     return it->second;
-  return flutter::EncodableValue();
+  return EncodableValue();
 }
 
-inline flutter::EncodableMap findMap(const flutter::EncodableMap& map,
-                                     const std::string& key) {
-  auto it = map.find(flutter::EncodableValue(key));
-  if (it != map.end() && TypeIs<flutter::EncodableMap>(it->second))
-    return GetValue<flutter::EncodableMap>(it->second);
-  return flutter::EncodableMap();
+inline EncodableMap findMap(const EncodableMap& map, const std::string& key) {
+  auto it = map.find(EncodableValue(key));
+  if (it != map.end() && TypeIs<EncodableMap>(it->second))
+    return GetValue<EncodableMap>(it->second);
+  return EncodableMap();
 }
 
-inline flutter::EncodableList findList(const flutter::EncodableMap& map,
-                                       const std::string& key) {
-  auto it = map.find(flutter::EncodableValue(key));
-  if (it != map.end() && TypeIs<flutter::EncodableList>(it->second))
-    return GetValue<flutter::EncodableList>(it->second);
-  return flutter::EncodableList();
+inline EncodableList findList(const EncodableMap& map, const std::string& key) {
+  auto it = map.find(EncodableValue(key));
+  if (it != map.end() && TypeIs<EncodableList>(it->second))
+    return GetValue<EncodableList>(it->second);
+  return EncodableList();
 }
 
-inline std::string findString(const flutter::EncodableMap& map,
-                              const std::string& key) {
-  auto it = map.find(flutter::EncodableValue(key));
+inline std::string findString(const EncodableMap& map, const std::string& key) {
+  auto it = map.find(EncodableValue(key));
   if (it != map.end() && TypeIs<std::string>(it->second))
     return GetValue<std::string>(it->second);
   return std::string();
 }
 
-inline int findInt(const flutter::EncodableMap& map, const std::string& key) {
-  auto it = map.find(flutter::EncodableValue(key));
+inline int findInt(const EncodableMap& map, const std::string& key) {
+  auto it = map.find(EncodableValue(key));
   if (it != map.end() && TypeIs<int>(it->second))
     return GetValue<int>(it->second);
   return -1;
 }
 
-inline double findDouble(const flutter::EncodableMap& map,
-                         const std::string& key) {
-  auto it = map.find(flutter::EncodableValue(key));
+inline double findDouble(const EncodableMap& map, const std::string& key) {
+  auto it = map.find(EncodableValue(key));
   if (it != map.end() && TypeIs<double>(it->second))
     return GetValue<double>(it->second);
   return 0.0;
 }
 
-inline int64_t findLongInt(const flutter::EncodableMap& map,
-                           const std::string& key) {
+inline int64_t findLongInt(const EncodableMap& map, const std::string& key) {
   for (auto it : map) {
     if (key == GetValue<std::string>(it.first)) {
       if (TypeIs<int64_t>(it.second)) {
@@ -98,19 +174,6 @@ inline int toInt(flutter::EncodableValue inputVal, int defaultVal) {
   return intValue;
 }
 
-typedef flutter::EncodableValue EncodableValue;
-typedef flutter::EncodableMap EncodableMap;
-typedef flutter::EncodableList EncodableList;
-
-typedef flutter::BinaryMessenger BinaryMessenger;
-typedef flutter::TextureRegistrar TextureRegistrar;
-typedef flutter::PluginRegistrar PluginRegistrar;
-typedef flutter::MethodChannel<EncodableValue> MethodChannel;
-typedef flutter::EventChannel<EncodableValue> EventChannel;
-typedef flutter::EventSink<EncodableValue> EventSink;
-typedef flutter::MethodCall<EncodableValue> MethodCall;
-typedef flutter::MethodResult<EncodableValue> MethodResult;
-
 class MethodCallProxy {
  public:
   static std::unique_ptr<MethodCallProxy> Create(const MethodCall& call);
@@ -124,7 +187,8 @@ class MethodCallProxy {
 
 class MethodResultProxy {
  public:
-  static std::unique_ptr<MethodResultProxy> Create(std::unique_ptr<MethodResult> method_result);
+  static std::unique_ptr<MethodResultProxy> Create(
+      std::unique_ptr<MethodResult> method_result);
 
   virtual ~MethodResultProxy() = default;
 
@@ -136,22 +200,26 @@ class MethodResultProxy {
 
   // Reports an error.
   virtual void Error(const std::string& error_code,
-             const std::string& error_message,
-             const EncodableValue* error_details) = 0;
+                     const std::string& error_message,
+                     const EncodableValue* error_details) = 0;
 
   // Reports an error with a default error code and no details.
-  virtual void Error(const std::string& error_code, const std::string& error_message = "") = 0;
+  virtual void Error(const std::string& error_code,
+                     const std::string& error_message = "") = 0;
 
   virtual void NotImplemented() = 0;
 };
 
 class EventChannelProxy {
  public:
-  static std::unique_ptr<EventChannelProxy> Create(BinaryMessenger* messenger, const std::string& channelName);
+  static std::unique_ptr<EventChannelProxy> Create(
+      BinaryMessenger* messenger,
+      const std::string& channelName);
 
   virtual ~EventChannelProxy() = default;
 
-  virtual void Success(const EncodableValue& event, bool cache_event = true) = 0;
+  virtual void Success(const EncodableValue& event,
+                       bool cache_event = true) = 0;
 };
 
 #endif  // FLUTTER_WEBRTC_COMMON_HXX
