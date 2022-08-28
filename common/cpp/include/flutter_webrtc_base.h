f9f055441754aaaf26203c0b1179d4c5324fa08f
cloudwebrtc <duanweiwei1982@gmail.com>
2022-08-28 08:15:53 +0800

    update.

--- common/cpp/include/flutter_webrtc_base.h
+++ common/cpp/include/flutter_webrtc_base.h
@@ -10,6 +10,8 @@
 #include <flutter/standard_method_codec.h>
 #include <flutter/texture_registrar.h>
 
+#include "flutter_common.h"
+
 #include <string.h>
 #include <list>
 #include <map>
@@ -34,88 +36,6 @@ using namespace flutter;
 class FlutterVideoRenderer;
 class FlutterRTCDataChannelObserver;
 class FlutterPeerConnectionObserver;
-
-// foo.StringValue() becomes std::get<std::string>(foo)
-// foo.IsString() becomes std::holds_alternative<std::string>(foo)
-
-template <typename T>
-inline bool TypeIs(const EncodableValue val) {
-  return std::holds_alternative<T>(val);
-}
-
-template <typename T>
-inline const T GetValue(EncodableValue val) {
-  return std::get<T>(val);
-}
-
-inline EncodableValue findEncodableValue(const EncodableMap& map,
-                                         const std::string& key) {
-  auto it = map.find(EncodableValue(key));
-  if (it != map.end())
-    return it->second;
-  return EncodableValue();
-}
-
-inline EncodableMap findMap(const EncodableMap& map, const std::string& key) {
-  auto it = map.find(EncodableValue(key));
-  if (it != map.end() && TypeIs<EncodableMap>(it->second))
-    return GetValue<EncodableMap>(it->second);
-  return EncodableMap();
-}
-
-inline EncodableList findList(const EncodableMap& map, const std::string& key) {
-  auto it = map.find(EncodableValue(key));
-  if (it != map.end() && TypeIs<EncodableList>(it->second))
-    return GetValue<EncodableList>(it->second);
-  return EncodableList();
-}
-
-inline std::string findString(const EncodableMap& map, const std::string& key) {
-  auto it = map.find(EncodableValue(key));
-  if (it != map.end() && TypeIs<std::string>(it->second))
-    return GetValue<std::string>(it->second);
-  return std::string();
-}
-
-inline int findInt(const EncodableMap& map, const std::string& key) {
-  auto it = map.find(EncodableValue(key));
-  if (it != map.end() && TypeIs<int>(it->second))
-    return GetValue<int>(it->second);
-  return -1;
-}
-
-inline double findDouble(const EncodableMap& map, const std::string& key) {
-  auto it = map.find(EncodableValue(key));
-  if (it != map.end() && TypeIs<double>(it->second))
-    return GetValue<double>(it->second);
-  return 0.0;
-}
-
-inline int64_t findLongInt(const EncodableMap& map, const std::string& key) {
-  for (auto it : map) {
-    if (key == GetValue<std::string>(it.first)) {
-      if (TypeIs<int64_t>(it.second)) {
-        return GetValue<int64_t>(it.second);
-      } else if (TypeIs<int32_t>(it.second)) {
-        return GetValue<int32_t>(it.second);
-      }
-    }
-  }
-
-  return -1;
-}
-
-inline int toInt(EncodableValue inputVal, int defaultVal) {
-  int intValue = defaultVal;
-  if (TypeIs<int>(inputVal)) {
-    intValue = GetValue<int>(inputVal);
-  } else if (TypeIs<int32_t>(inputVal)) {
-    intValue = GetValue<int32_t>(inputVal);
-  } else if (TypeIs<std::string>(inputVal)) {
-    intValue = atoi(GetValue<std::string>(inputVal).c_str());
-  }
-  return intValue;
-}
 
 class FlutterWebRTCBase {
  public:
