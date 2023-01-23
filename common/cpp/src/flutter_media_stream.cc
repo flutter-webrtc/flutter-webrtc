3f1bdda064004d7859a9663f498d61bc84e546e5
Vlad Koronnov <vladimir.koronnov@infotecs.ru>
2023-01-23 18:19:52 +0400

    Fixed displaing of short device names for windows

--- common/cpp/src/flutter_media_stream.cc
+++ common/cpp/src/flutter_media_stream.cc
@@ -298,15 +298,15 @@ void FlutterMediaStream::GetSources(std::unique_ptr<MethodResultProxy> result) {
   EncodableList sources;
 
   int nb_audio_devices = base_->audio_device_->RecordingDevices();
-  char strNameUTF8[RTCAudioDevice::kAdmMaxDeviceNameSize];
-  char strGuidUTF8[RTCAudioDevice::kAdmMaxGuidSize];
+  char strNameUTF8[RTCAudioDevice::kAdmMaxDeviceNameSize + 1] = {0};
+  char strGuidUTF8[RTCAudioDevice::kAdmMaxGuidSize + 1] = {0};
 
   for (uint16_t i = 0; i < nb_audio_devices; i++) {
     base_->audio_device_->RecordingDeviceName(i, strNameUTF8, strGuidUTF8);
     EncodableMap audio;
-    audio[EncodableValue("label")] = EncodableValue(std::string(strNameUTF8, RTCAudioDevice::kAdmMaxDeviceNameSize));
+    audio[EncodableValue("label")] = EncodableValue(std::string(strNameUTF8));
     audio[EncodableValue("deviceId")] =
-        EncodableValue(std::string(strGuidUTF8, RTCAudioDevice::kAdmMaxGuidSize));
+        EncodableValue(std::string(strGuidUTF8));
     audio[EncodableValue("facing")] = "";
     audio[EncodableValue("kind")] = "audioinput";
     sources.push_back(EncodableValue(audio));
@@ -316,9 +316,9 @@ void FlutterMediaStream::GetSources(std::unique_ptr<MethodResultProxy> result) {
   for (uint16_t i = 0; i < nb_audio_devices; i++) {
     base_->audio_device_->PlayoutDeviceName(i, strNameUTF8, strGuidUTF8);
     EncodableMap audio;
-    audio[EncodableValue("label")] = EncodableValue(std::string(strNameUTF8, RTCAudioDevice::kAdmMaxDeviceNameSize));
+    audio[EncodableValue("label")] = EncodableValue(std::string(strNameUTF8));
     audio[EncodableValue("deviceId")] =
-        EncodableValue(std::string(strGuidUTF8, RTCAudioDevice::kAdmMaxGuidSize));
+        EncodableValue(std::string(strGuidUTF8));
     audio[EncodableValue("facing")] = "";
     audio[EncodableValue("kind")] = "audiooutput";
     sources.push_back(EncodableValue(audio));
@@ -328,9 +328,9 @@ void FlutterMediaStream::GetSources(std::unique_ptr<MethodResultProxy> result) {
   for (int i = 0; i < nb_video_devices; i++) {
     base_->video_device_->GetDeviceName(i, strNameUTF8, 128, strGuidUTF8, 128);
     EncodableMap video;
-    video[EncodableValue("label")] = EncodableValue(std::string(strNameUTF8, 128));
+    video[EncodableValue("label")] = EncodableValue(std::string(strNameUTF8));
     video[EncodableValue("deviceId")] =
-        EncodableValue(std::string(strGuidUTF8, 128));
+        EncodableValue(std::string(strGuidUTF8));
     video[EncodableValue("facing")] = i == 1 ? "front" : "back";
     video[EncodableValue("kind")] = "videoinput";
     sources.push_back(EncodableValue(video));
