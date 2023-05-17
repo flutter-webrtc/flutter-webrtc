35299abda231c6d91ab0604612bece61f285de13
cloudwebrtc <duanweiwei1982@gmail.com>
2023-05-17 08:57:43 +0800

    add default examples.

---
+++ example/ios/RunnerTests/RunnerTests.m
@@ -0,0 +1,16 @@
+#import <Flutter/Flutter.h>
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface RunnerTests : XCTestCase
+
+@end
+
+@implementation RunnerTests
+
+- (void)testExample {
+  // If you add code to the Runner application, consider adding tests here.
+  // See https://developer.apple.com/documentation/xctest for more information about using XCTest.
+}
+
+@end
