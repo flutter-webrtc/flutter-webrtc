efe6af1b311b9a0b62eb0f66046b9c7599f15fd6
cloudwebrtc <duanweiwei1982@gmail.com>
2023-06-29 17:25:57 +0800

    windows.

--- third_party/libwebrtc/include/rtc_frame_cryptor.h
+++ third_party/libwebrtc/include/rtc_frame_cryptor.h
@@ -42,7 +42,8 @@ class KeyProvider : public RefCountInterface {
                                      int key_index) = 0;
 
   virtual vector<uint8_t> ExportKey(const string participant_id,
-                                     int key_index) = 0;
+                                    int key_index) = 0;
+
  protected:
   virtual ~KeyProvider() {}
 };
