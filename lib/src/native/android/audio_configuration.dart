65af5becfd31f108ab488a150b4270d1985d8e6e
davidliu <davidliu@deviange.net>
2023-08-30 13:25:38 +0900

    Add more control over android audio options (#1434)

--- lib/src/native/android/audio_configuration.dart
+++ lib/src/native/android/audio_configuration.dart
@@ -23,8 +23,7 @@ enum AndroidAudioFocusMode {
   gain,
   gainTransient,
   gainTransientExclusive,
-  gainTransientMayDuck,
-  loss
+  gainTransientMayDuck
 }
 
 extension AndroidAudioFocusModeExt on AndroidAudioFocusMode {
@@ -37,20 +36,127 @@ extension AndroidAudioFocusModeEnumEx on String {
           .firstWhere((d) => describeEnum(d) == toLowerCase());
 }
 
+enum AndroidAudioStreamType {
+  accessibility,
+  alarm,
+  dtmf,
+  music,
+  notification,
+  ring,
+  system,
+  voiceCall
+}
+
+extension AndroidAudioStreamTypeExt on AndroidAudioStreamType {
+  String get value => describeEnum(this);
+}
+
+extension AndroidAudioStreamTypeEnumEx on String {
+  AndroidAudioStreamType toAndroidAudioStreamType() =>
+      AndroidAudioStreamType.values
+          .firstWhere((d) => describeEnum(d) == toLowerCase());
+}
+
+enum AndroidAudioAttributesUsageType {
+  alarm,
+  assistanceAccessibility,
+  assistanceNavigationGuidance,
+  assistanceSonification,
+  assistant,
+  game,
+  media,
+  notification,
+  notificationEvent,
+  notificationRingtone,
+  unknown,
+  voiceCommunication,
+  voiceCommunicationSignalling
+}
+
+extension AndroidAudioAttributesUsageTypeExt
+    on AndroidAudioAttributesUsageType {
+  String get value => describeEnum(this);
+}
+
+extension AndroidAudioAttributesUsageTypeEnumEx on String {
+  AndroidAudioAttributesUsageType toAndroidAudioAttributesUsageType() =>
+      AndroidAudioAttributesUsageType.values
+          .firstWhere((d) => describeEnum(d) == toLowerCase());
+}
+
+enum AndroidAudioAttributesContentType {
+  movie,
+  music,
+  sonification,
+  speech,
+  unknown
+}
+
+extension AndroidAudioAttributesContentTypeExt
+    on AndroidAudioAttributesContentType {
+  String get value => describeEnum(this);
+}
+
+extension AndroidAudioAttributesContentTypeEnumEx on String {
+  AndroidAudioAttributesContentType toAndroidAudioAttributesContentType() =>
+      AndroidAudioAttributesContentType.values
+          .firstWhere((d) => describeEnum(d) == toLowerCase());
+}
+
 class AndroidAudioConfiguration {
   AndroidAudioConfiguration({
+    this.manageAudioFocus,
     this.androidAudioMode,
     this.androidAudioFocusMode,
+    this.androidAudioStreamType,
+    this.androidAudioAttributesUsageType,
+    this.androidAudioAttributesContentType,
   });
+
+  final bool? manageAudioFocus;
   final AndroidAudioMode? androidAudioMode;
   final AndroidAudioFocusMode? androidAudioFocusMode;
+  final AndroidAudioStreamType? androidAudioStreamType;
+  final AndroidAudioAttributesUsageType? androidAudioAttributesUsageType;
+  final AndroidAudioAttributesContentType? androidAudioAttributesContentType;
 
   Map<String, dynamic> toMap() => <String, dynamic>{
+        if (manageAudioFocus != null) 'manageAudioFocus': manageAudioFocus!,
         if (androidAudioMode != null)
           'androidAudioMode': androidAudioMode!.value,
         if (androidAudioFocusMode != null)
           'androidAudioFocusMode': androidAudioFocusMode!.value,
+        if (androidAudioStreamType != null)
+          'androidAudioStreamType': androidAudioStreamType!.value,
+        if (androidAudioAttributesUsageType != null)
+          'androidAudioAttributesUsageType':
+              androidAudioAttributesUsageType!.value,
+        if (androidAudioAttributesContentType != null)
+          'androidAudioAttributesContentType':
+              androidAudioAttributesContentType!.value,
       };
+
+  /// A pre-configured AndroidAudioConfiguration for media playback.
+  static final media = AndroidAudioConfiguration(
+    manageAudioFocus: true,
+    androidAudioMode: AndroidAudioMode.normal,
+    androidAudioFocusMode: AndroidAudioFocusMode.gain,
+    androidAudioStreamType: AndroidAudioStreamType.music,
+    androidAudioAttributesUsageType: AndroidAudioAttributesUsageType.media,
+    androidAudioAttributesContentType:
+        AndroidAudioAttributesContentType.unknown,
+  );
+
+  /// A pre-configured AndroidAudioConfiguration for voice communication.
+  static final communication = AndroidAudioConfiguration(
+    manageAudioFocus: true,
+    androidAudioMode: AndroidAudioMode.inCommunication,
+    androidAudioFocusMode: AndroidAudioFocusMode.gain,
+    androidAudioStreamType: AndroidAudioStreamType.voiceCall,
+    androidAudioAttributesUsageType:
+        AndroidAudioAttributesUsageType.voiceCommunication,
+    androidAudioAttributesContentType: AndroidAudioAttributesContentType.speech,
+  );
 }
 
 class AndroidNativeAudioManagement {
