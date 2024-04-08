23018e04604e1ad5fecaecd3fbe54b5485e89388
cloudwebrtc <duanweiwei1982@gmail.com>
2024-04-08 19:01:44 +0800

    fix.

--- lib/src/web/rtc_video_renderer_impl.dart
+++ lib/src/web/rtc_video_renderer_impl.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:js_interop';
 import 'dart:js_util' as jsutil;
-import 'dart:ui_web' as ui;
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
