717ea644c88f1a594216e23733847a55fa04bdec
cloudwebrtc <duanweiwei1982@gmail.com>
2024-04-09 10:57:02 +0800

    Fix compilation warning for iOS/macOS.

--- common/darwin/Classes/FlutterWebRTCPlugin.h
+++ common/darwin/Classes/FlutterWebRTCPlugin.h
@@ -10,11 +10,11 @@
 @class FlutterRTCVideoRenderer;
 @class FlutterRTCFrameCapturer;
 
-void postEvent(FlutterEventSink sink, id _Nullable event);
+void postEvent(FlutterEventSink _Nonnull sink, id _Nullable event);
 
 typedef void (^CompletionHandler)(void);
 
-typedef void (^CapturerStopHandler)(CompletionHandler handler);
+typedef void (^CapturerStopHandler)(CompletionHandler _Nonnull handler);
 
 @interface FlutterWebRTCPlugin : NSObject <FlutterPlugin,
                                            RTCPeerConnectionDelegate,
@@ -26,43 +26,43 @@ typedef void (^CapturerStopHandler)(CompletionHandler handler);
 #endif
                                            >
 
-@property(nonatomic, strong) RTCPeerConnectionFactory* peerConnectionFactory;
-@property(nonatomic, strong) NSMutableDictionary<NSString*, RTCPeerConnection*>* peerConnections;
-@property(nonatomic, strong) NSMutableDictionary<NSString*, RTCMediaStream*>* localStreams;
-@property(nonatomic, strong) NSMutableDictionary<NSString*, RTCMediaStreamTrack*>* localTracks;
-@property(nonatomic, strong) NSMutableDictionary<NSNumber*, FlutterRTCVideoRenderer*>* renders;
+@property(nonatomic, strong) RTCPeerConnectionFactory* _Nullable peerConnectionFactory;
+@property(nonatomic, strong) NSMutableDictionary<NSString*, RTCPeerConnection*>* _Nullable peerConnections;
+@property(nonatomic, strong) NSMutableDictionary<NSString*, RTCMediaStream*>* _Nullable localStreams;
+@property(nonatomic, strong) NSMutableDictionary<NSString*, RTCMediaStreamTrack*>* _Nullable localTracks;
+@property(nonatomic, strong) NSMutableDictionary<NSNumber*, FlutterRTCVideoRenderer*>* _Nullable renders;
 @property(nonatomic, strong)
-    NSMutableDictionary<NSString*, CapturerStopHandler>* videoCapturerStopHandlers;
+    NSMutableDictionary<NSString*, CapturerStopHandler>* _Nullable videoCapturerStopHandlers;
 
-@property(nonatomic, strong) NSMutableDictionary<NSString*, RTCFrameCryptor*>* frameCryptors;
-@property(nonatomic, strong) NSMutableDictionary<NSString*, RTCFrameCryptorKeyProvider*>* keyProviders;
+@property(nonatomic, strong) NSMutableDictionary<NSString*, RTCFrameCryptor*>* _Nullable frameCryptors;
+@property(nonatomic, strong) NSMutableDictionary<NSString*, RTCFrameCryptorKeyProvider*>* _Nullable keyProviders;
 
 #if TARGET_OS_IPHONE
 @property(nonatomic, retain) UIViewController* viewController; /*for broadcast or ReplayKit */
 #endif
 
-@property(nonatomic, strong) FlutterEventSink eventSink;
-@property(nonatomic, strong) NSObject<FlutterBinaryMessenger>* messenger;
-@property(nonatomic, strong) RTCCameraVideoCapturer* videoCapturer;
-@property(nonatomic, strong) FlutterRTCFrameCapturer* frameCapturer;
-@property(nonatomic, strong) AVAudioSessionPort preferredInput;
+@property(nonatomic, strong) FlutterEventSink _Nullable eventSink;
+@property(nonatomic, strong) NSObject<FlutterBinaryMessenger>* _Nonnull messenger;
+@property(nonatomic, strong) RTCCameraVideoCapturer* _Nullable videoCapturer;
+@property(nonatomic, strong) FlutterRTCFrameCapturer* _Nullable frameCapturer;
+@property(nonatomic, strong) AVAudioSessionPort _Nullable preferredInput;
 @property(nonatomic) BOOL _usingFrontCamera;
 @property(nonatomic) NSInteger _lastTargetWidth;
 @property(nonatomic) NSInteger _lastTargetHeight;
 @property(nonatomic) NSInteger _lastTargetFps;
 
-- (RTCMediaStream*)streamForId:(NSString*)streamId peerConnectionId:(NSString*)peerConnectionId;
-- (RTCRtpTransceiver*)getRtpTransceiverById:(RTCPeerConnection*)peerConnection Id:(NSString*)Id;
-- (NSDictionary*)mediaStreamToMap:(RTCMediaStream*)stream ownerTag:(NSString*)ownerTag;
-- (NSDictionary*)mediaTrackToMap:(RTCMediaStreamTrack*)track;
-- (NSDictionary*)receiverToMap:(RTCRtpReceiver*)receiver;
-- (NSDictionary*)transceiverToMap:(RTCRtpTransceiver*)transceiver;
+- (RTCMediaStream* _Nullable)streamForId:(NSString* _Nonnull)streamId peerConnectionId:(NSString* _Nonnull)peerConnectionId;
+- (RTCRtpTransceiver* _Nullable)getRtpTransceiverById:(RTCPeerConnection* _Nonnull)peerConnection Id:(NSString* _Nonnull)Id;
+- (NSDictionary* _Nullable)mediaStreamToMap:(RTCMediaStream* _Nonnull)stream ownerTag:(NSString* _Nonnull)ownerTag;
+- (NSDictionary* _Nullable)mediaTrackToMap:(RTCMediaStreamTrack* _Nonnull)track;
+- (NSDictionary* _Nullable)receiverToMap:(RTCRtpReceiver* _Nonnull)receiver;
+- (NSDictionary* _Nullable)transceiverToMap:(RTCRtpTransceiver* _Nonnull)transceiver;
 
 - (BOOL)hasLocalAudioTrack;
 - (void)ensureAudioSession;
 - (void)deactiveRtcAudioSession;
 
-- (RTCRtpReceiver*)getRtpReceiverById:(RTCPeerConnection*)peerConnection Id:(NSString*)Id;
-- (RTCRtpSender*)getRtpSenderById:(RTCPeerConnection*)peerConnection Id:(NSString*)Id;
+- (RTCRtpReceiver* _Nullable)getRtpReceiverById:(RTCPeerConnection* _Nonnull)peerConnection Id:(NSString* _Nonnull)Id;
+- (RTCRtpSender* _Nullable)getRtpSenderById:(RTCPeerConnection* _Nonnull)peerConnection Id:(NSString* _Nonnull)Id;
 
 @end
