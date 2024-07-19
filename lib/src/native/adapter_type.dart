a50030f285052f3aeeb14b7f9e77600d92f06e1d
cloudwebrtc <duanweiwei1982@gmail.com>
2024-07-19 14:14:34 +0800

    fixed analyzing issues.

--- lib/src/native/adapter_type.dart
+++ lib/src/native/adapter_type.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/foundation.dart';
-
 enum AdapterType {
   adapterTypeUnknown,
   adapterTypeEthernet,
--- lib/src/native/ios/audio_configuration.dart
+++ lib/src/native/ios/audio_configuration.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/foundation.dart';
-
 import '../utils.dart';
 
 enum AppleAudioMode {
