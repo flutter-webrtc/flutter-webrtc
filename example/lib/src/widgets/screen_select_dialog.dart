af6595d396965b2750151904ada6a646627d2126
CloudWebRTC <duanweiwei1982@gmail.com>
2024-08-07 15:12:56 +0800

    fix: PlatformView invisible back from background (#1647)
    * fix bug for PlatformView.
    
    * bump version.
    
    * fix compile for android.
    
    * fix

--- example/lib/src/widgets/screen_select_dialog.dart
+++ example/lib/src/widgets/screen_select_dialog.dart
@@ -279,7 +279,7 @@ class ScreenSelectDialog extends Dialog {
             ),
             Container(
               width: double.infinity,
-              child: ButtonBar(
+              child: OverflowBar(
                 children: <Widget>[
                   MaterialButton(
                     child: Text(
