f98972a73779edd0e17f9e44dfe476cb59bd46ea
thiesencadu <cadu@zensleep.ai>
2024-08-23 15:45:01 -0300

    selectAudioOutput and selectAudioInput methods created to enable dynamic change on audio intput/output device

--- lib/src/native/media_stream_track_impl.dart
+++ lib/src/native/media_stream_track_impl.dart
@@ -84,6 +84,16 @@ class MediaStreamTrackNative extends MediaStreamTrack {
   }
 
   @override
+  void selectAudioOutput(String deviceId) async {
+    return Helper.selectAudioOutput(deviceId);
+  }
+
+  @override
+  void selectAudioInput(String deviceId) async {
+    return Helper.selectAudioInput(deviceId);
+  }
+
+  @override
   Future<ByteBuffer> captureFrame() async {
     var filePath = await getTemporaryDirectory();
     await WebRTC.invokeMethod(
