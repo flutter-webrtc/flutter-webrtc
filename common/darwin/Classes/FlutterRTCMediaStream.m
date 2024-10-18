45754085d07815db98c78a7647171bb5bc3d64ba
CloudWebRTC <duanweiwei1982@gmail.com>
2024-10-18 10:53:49 +0800

    Update FlutterRTCMediaStream.m

--- common/darwin/Classes/FlutterRTCMediaStream.m
+++ common/darwin/Classes/FlutterRTCMediaStream.m
@@ -922,11 +922,13 @@ typedef void (^NavigatorUserMediaSuccessCallback)(RTCMediaStream* mediaStream);
   for (AVCaptureDeviceFormat* format in formats) {
     CMVideoDimensions dimension = CMVideoFormatDescriptionGetDimensions(format.formatDescription);
     FourCharCode pixelFormat = CMFormatDescriptionGetMediaSubType(format.formatDescription);
+#if TARGET_OS_IPHONE
     if (@available(iOS 13.0, *)) {
       if(format.isMultiCamSupported != AVCaptureMultiCamSession.multiCamSupported) {
         continue;
       }
     }
+#endif
     //NSLog(@"AVCaptureDeviceFormats,fps %d, dimension: %dx%d", format.videoSupportedFrameRateRanges, dimension.width, dimension.height);
     long diff = labs(targetWidth - dimension.width) + labs(targetHeight - dimension.height);
     if (diff < currentDiff) {
