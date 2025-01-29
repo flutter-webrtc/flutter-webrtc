e6de1a6b15ee210188ac50ef95638047c74e3864
holzgeist <holzgeist@users.noreply.github.com>
2025-01-29 12:21:47 +0100

    feat: expose rtc video value (#1754)
    depends on https://github.com/flutter-webrtc/webrtc-interface/pull/30

--- lib/src/native/rtc_video_renderer_impl.dart
+++ lib/src/native/rtc_video_renderer_impl.dart
@@ -39,6 +39,9 @@ class RTCVideoRenderer extends ValueNotifier<RTCVideoValue>
   int get videoHeight => value.height.toInt();
 
   @override
+  RTCVideoValue get videoValue => value;
+
+  @override
   int? get textureId => _textureId;
 
   @override
