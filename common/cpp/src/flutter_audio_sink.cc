834c392661f173822de8504e2077478195fd5622
cloudwebrtc <duanweiwei1982@gmail.com>
2025-06-23 11:09:48 +0800

    update.

---
+++ common/cpp/src/flutter_audio_sink.cc
@@ -0,0 +1,5 @@
+#include "flutter_audio_sink.h"
+
+namespace flutter_webrtc_plugin {
+
+} // namespace flutter_webrtc_plugin
--- common/darwin/Classes/FlutterRTCAudioSink-Interface.h
+++ common/darwin/Classes/FlutterRTCAudioSink-Interface.h
@@ -1,6 +1,11 @@
+#ifndef FLUTTER_RTC_AUDIO_SINK_INTERFACE_H
+#define FLUTTER_RTC_AUDIO_SINK_INTERFACE_H
+
 void RTCAudioSinkCallback (void *object,
                            const void *audio_data,
                            int bits_per_sample,
                            int sample_rate,
                            size_t number_of_channels,
                            size_t number_of_frames);
+
+#endif // FLUTTER_RTC_AUDIO_SINK_INTERFACE_H
