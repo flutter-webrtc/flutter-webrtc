83b14c770a04d93ee61b95b8f2171cfdd47c9317
Ökkeş Aksoy <49321543+Aksoyhlc@users.noreply.github.com>
2026-02-17 11:54:12 +0300

    fix(android): DegradationPreference valueOf fails in Turkish locale (use Locale.US in toUpperCase)

--- android/src/main/java/com/cloudwebrtc/webrtc/PeerConnectionObserver.java
+++ android/src/main/java/com/cloudwebrtc/webrtc/PeerConnectionObserver.java
@@ -25,6 +25,7 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.UUID;
 
 import org.webrtc.AudioTrack;
@@ -781,7 +782,7 @@ class PeerConnectionObserver implements PeerConnection.Observer, EventChannel.St
 
     String degradationPreference = (String) newParameters.get("degradationPreference");
     if (degradationPreference != null) {
-      parameters.degradationPreference = RtpParameters.DegradationPreference.valueOf(degradationPreference.toUpperCase().replace("-", "_"));
+      parameters.degradationPreference = RtpParameters.DegradationPreference.valueOf(degradationPreference.toUpperCase(Locale.US).replace("-", "_"));
     }
 
     for (Map<String, Object> encoding : encodings) {
