cd4c8efffbd7e09e566735c2f761ff76bc27b025
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2026-09-11 18:04:40 +0800

    test: satisfy import_sorter in the data channel dispose test
    The CI import sorter groups third party packages together with no
    blank line between them.

--- test/unit/rtc_peerconnection_dispose_data_channels_test.dart
+++ test/unit/rtc_peerconnection_dispose_data_channels_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/services.dart';
 
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:webrtc_interface/webrtc_interface.dart';
 
 import 'package:flutter_webrtc/src/native/rtc_data_channel_impl.dart';
