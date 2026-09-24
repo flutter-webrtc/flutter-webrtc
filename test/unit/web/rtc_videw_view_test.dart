a350d5f021aeeb1f62bad65b1081c5d456532c96
Bruno D'Luka <brunodlukaa@gmail.com>
2026-09-24 17:44:09 -0300

    style(web): sort video view test imports

--- test/unit/web/rtc_videw_view_test.dart
+++ test/unit/web/rtc_videw_view_test.dart
@@ -4,6 +4,7 @@ library;
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_webrtc/flutter_webrtc.dart';
+
 import 'package:flutter_webrtc/src/web/rtc_video_renderer_impl.dart'
     show useHtmlElementView;
 
