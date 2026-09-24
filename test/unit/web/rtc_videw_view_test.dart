d6a5fcda57abd6686f004a823f226e125e8a099e
Bruno D'Luka <brunodlukaa@gmail.com>
2026-09-24 17:00:21 -0300

    fix(web): prevent native video interactions in HTML views

--- test/unit/web/rtc_videw_view_test.dart
+++ test/unit/web/rtc_videw_view_test.dart
@@ -4,8 +4,29 @@ library;
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_webrtc/flutter_webrtc.dart';
+import 'package:flutter_webrtc/src/web/rtc_video_renderer_impl.dart'
+    show useHtmlElementView;
 
 void main() {
+  test('video is an autoplaying, non-interactive render surface', () {
+    final renderer = RTCVideoRenderer();
+    final video = renderer.createElement();
+    expect(video.autoplay, isTrue);
+    expect(video.controls, isFalse);
+    expect(video.hasAttribute('playsinline'), isTrue);
+    expect(video.style.pointerEvents, 'none');
+    expect(video.hasAttribute('disablepictureinpicture'), isTrue);
+    expect(video.hasAttribute('disableremoteplayback'), isTrue);
+    expect(
+      video.getAttribute('controlsList'),
+      'nodownload nofullscreen noremoteplayback',
+    );
+    if (useHtmlElementView) {
+      expect(video.style.userSelect, 'none');
+    }
+    renderer.dispose();
+  });
+
   // TODO(wer-mathurin): should revisit after this bug is resolved, https://github.com/flutter/flutter/issues/66045.
   test('should complete succesfully', () async {
     var renderer = RTCVideoRenderer();
